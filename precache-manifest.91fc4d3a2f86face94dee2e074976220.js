394110cf6103decc2da4c612427a62e03770b0fb
Umaru Barrie <umaru.barrie@student.vives.be>
2020-07-04 00:15:51 +0200

    Updates

---
+++ precache-manifest.91fc4d3a2f86face94dee2e074976220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103bbea035caf762e8424e007ea7c79c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "24ce97be2b713b47d587",
+    "url": "/robofriends/static/css/main.b0a97c3d.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ce97be2b713b47d587",
+    "url": "/robofriends/static/js/main.7e7005e8.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
